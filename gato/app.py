69bfdf5cd15d2f8c170fd3f0177875af018a8855
arturocordovaaa <luisarturoc952@gmail.com>
2025-02-07 11:01:00 -0700

    gato ya tiene contador de ganados, perdidos y empates

--- gato/app.py
+++ gato/app.py
@@ -1,19 +1,24 @@
 '''
-Este archivo es el punto de entrada de la aplicacion
+Este archivo es el punto de entrada a la aplicación. Aquí se importan las funciones de tablero.py y se ejecutan en un ciclo while.
 '''
 import tablero
 
 def main():
-    '''
-    Funcion principal
-    '''
-    numeros = [str(x) for x in range(1,10) ]
-    dsimbolos = {x:x for x in numeros}
-    g = tablero.juego(dsimbolos)
-    if g is not None:
-        print(f'El ganador es {g}')
-    else:
-        print('Empate')
-    
+    ''' Función principal '''
+    X = {"G":0,"P":0,"E":0}
+    O = {"G":0,"P":0,"E":0}
+    score = {"X":X,"O":O}
+    numeros =[str(i) for i in range(1,10)]
+    corriendo = True
+
+    while corriendo:
+        dsimbolos = {x:x for x in numeros}
+        g = tablero.juego(dsimbolos)
+        tablero.actualiza_score(score,g)
+        tablero.despliega_tablero(score)
+        seguir = input('¿Quieres seguir? (s/n): ')
+        if seguir.lower() == 'n':
+            corriendo = False
+
 if __name__ == '__main__':
     main()
