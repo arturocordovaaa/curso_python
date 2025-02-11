81938f1c6d61a91c6cd28bf70df1619b80caee5f
arturocordovaaa <luisarturoc952@gmail.com>
2025-02-10 17:01:19 -0700

    ahorcado imprime imagen

---
+++ ahorcado/funciones.py
@@ -0,0 +1,16 @@
+'''
+funciones auxiliares del juego del ahorcado
+'''
+
+def carga_archivo_texto(archivo:str)->list:
+    '''
+    carga un archivo de texto y devuelve una lista on las orcaiones del archivo
+    '''
+    with open(archivo, 'r') as file:
+        oraciones = file.readlines()
+    return oraciones
+
+if __name__ == '__main__':
+    lista = carga_archivo_texto("./plantilla/plantilla-0.txt")
+    for elemento in lista:
+        print(elemento)
