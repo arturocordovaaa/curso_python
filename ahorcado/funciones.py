71c57e903b46a87d38337dd8db0e7bcd926a36c3
arturocordovaaa <luisarturoc952@gmail.com>
2025-02-11 19:29:25 -0700

    ahorcado corregido

--- ahorcado/funciones.py
+++ ahorcado/funciones.py
@@ -1,16 +1,34 @@
 '''
-funciones auxiliares del juego del ahorcado
+funciones auxiliares del juego Ahorcado
 '''
-
-def carga_archivo_texto(archivo:str)->list:
+ 
+def carga_archivo_texto(archivo:str)-> list:
     '''
-    carga un archivo de texto y devuelve una lista on las orcaiones del archivo
+    Carga un archivo de texto y devuelveuna lista con las oraciones del archivo
     '''
-    with open(archivo, 'r') as file:
+ 
+    with open(archivo,'r') as file:
         oraciones = file.readlines()
-    return oraciones
-
+    return oraciones  
+ 
+def carga_plantillas(nombre_plantilla)->dict:
+    '''
+    Carga las plantillas del juego a partir de un archivo de un texto
+    '''  
+    plantillas = {}
+    for i in range(5):
+        plantillas[i] = carga_archivo_texto(f'./plantillas/{nombre_plantilla}-{i}.txt')
+    return plantillas
+ 
+def despliega_plantilla(diccionario:dict, nivel:int):
+    '''
+    Despliega una pklanitilla del juego
+    '''
+    plantilla = diccionario[nivel]
+    for renglon in plantilla:
+        print(renglon)
+ 
 if __name__ == '__main__':
-    lista = carga_archivo_texto("c:/ESCUELA/desarrollo 4/curso_python-1/ahorcado/plantilla/plantilla-0.txt")
-    for elemento in lista:
-        print(elemento)
+    plantilla = carga_plantillas('plantilla')
+    despliega_plantilla(plantilla,0)
+    despliega_plantilla(plantilla,1)
