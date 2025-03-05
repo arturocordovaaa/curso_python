6feafa226f7eed2b4c262d8450428e641a2dc717
arturocordovaaa <luisarturoc952@gmail.com>
2025-03-05 10:39:08 -0700

    hello world ya funciona!

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -2,13 +2,13 @@ from flask import Flask
  
 app = Flask(__name__)
  
-@app.route('/')
+@app.route("/") # home or root of website
 def index():
-    return '<html><head><title>Hello World</title></head><body><h1>Hello World</h1> <p> Ir a <a href="/about">about</a></p></body></html>'
+    return '<html><head><title>HELLO WORLD</title></head><body><h1>Hello World</h1><p>Ir a <a href="/about">about</a></p></body></html>'
  
-@app.route('/about')
+@app.route("/about") # info about this site
 def about():
-    '<html><head><title>About this page </title></head><body> Everything about this website. Back to <a href="/"> Hello World </a></body></html>'
+    return '<html><head><title>About this page</title></head><body>Everythinf about this website. Back to <a href="/">Hello World</a></body></html>'
  
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
