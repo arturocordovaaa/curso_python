fb596543cc157859287365817baa1349a6a175d7
arturocordovaaa <luisarturoc952@gmail.com>
2025-03-20 11:03:11 -0700

    ya funciona login

--- libros_web/app.py
+++ libros_web/app.py
@@ -13,9 +13,9 @@ diccionario_id = fn.crea_diccionario(lista_libros,'id')
 @app.route('/')
 def inicio():
     ''' Página de inicio de la aplicación '''
-    return render_template('index.html')
+    return render_template('inicio.html')
 
-@app.route('/titulo', methods=['GET','POST'])
+@app.route('/titulos', methods=['GET','POST'])
 def busqueda_titulo():
     ''' Página de búsqueda por título '''
     resultado = []
@@ -24,16 +24,37 @@ def busqueda_titulo():
         resultado = fn.busca_en_diccionario(diccionario_titulos, titulo)
         print(titulo)
         print(resultado)
-    return render_template('titulo.html', lista_libros=resultado)
+    return render_template('titulos.html', lista_libros=resultado)
 
 @app.route('/libro/<id_libro>', methods =['GET'])
 def libro(id_libro:str):
     '''Pagina de informacion de un libro'''
     if id_libro in diccionario_id:
         book = diccionario_id[id_libro]
-        return render_template('libro.html', libro=book)
+        return render_template('libros.html', libro=book)
     else:
-        return render_template('libro.html', libro=None)
+        return render_template('libros.html', libro=None)
+
+@app.route('/titulo', methods = ['GET','POST'])
+def title():
+    '''Pagina de busqueda por titulo'''
+    print(request.method)
+    resultado=[]
+    if request.method == 'POST':
+        titulo = request.form.get('searchInput','')
+        resultado = fn.busca_en_diccionario(diccionario_titulos,titulo)
+    return render_template('titulos.html', lista_libros=resultado)
+
+@app.route('/letra/', methods =['GET'])
+def plantilla_letra():
+    '''Pagina de busqueda de letra'''
+    return render_template('por_letra.html')
+
+@app.route('/letra/<letra>', methods =['GET'])
+def busqueda_letra(letra:str):
+    '''Pagina de busqueda por letra'''
+    resultado = fn.libros_empiezan_con(lista_libros, letra)
+    return render_template('por_letra.html', lista_libros=resultado)
 
 if __name__ == '__main__':
     app.run(debug=True)
